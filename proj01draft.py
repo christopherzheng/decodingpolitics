e618904f62c697798fe5899ae1f56bdbea5a4527
Christopher Zheng <c.zheng@berkeley.edu>
2018-12-20 17:00:34 -0500

    initial commit

---
+++ proj01draft.py
@@ -0,0 +1,31 @@
+"""
+In this project, you'll determine how many times are you, the last voter
+of an election is the deciding vode of the election. You will write code
+that simulates an election many times and it can handle v voters and c
+candidates. In other words, your code should be able to handle any 
+arbitrary number of voters and number of candidates. 
+"""
+
+## TODO: Write a function that determines which candidate has the most 
+## votes. 
+
+## TODO: Write a function that determines if there's a tie between any
+## two candidates in the election.
+
+## TODO: Write a function that determine if you're the deciding vote.  
+
+## TODO: Write a function that acts as one election.
+
+## TODO: Write a function that simulates the election an arbitrary number
+## of times. 
+
+def simulation(num, voters, candidates): 
+	return True
+
+
+def main(): 
+	## TODO: Call the simulation with an appropriate number of times. 
+
+	## SET THIS UP SO THAT THEY RUN MULTIPLE SIMULATIONS AND COMPARE THE RESULTS.
+
+main()
---
+++ proj03draft.py
@@ -0,0 +1,5 @@
+""" In this project, you will randomly create tweets of 280 characters. We will define a character as any button
+pressed on a keyboard that results in the cursor moving. In other words, this means all letters, all numbers, all
+special characters (e.g. !#$@"), and, importantly, the space. While there is a way to do this project using
+advanced computer science techniques such as natural language processing, we thought it would be fun to see if you
+are able to randomly select words to form a 280-character tweet that is grammatically correct 
